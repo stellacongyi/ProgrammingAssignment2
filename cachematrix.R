d730df9fedd30c2bc508c9e0f1b89db6d791a027
STELLA <cz2402@columbia.edu>
2016-07-07 20:12:53 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+Inverse = solve(x)
+  list(S_matrix = x,
+       Inverse = Inverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m = x$Inverse    ## Return a matrix that is the inverse of 'x'
+  if(!is.na(m)){
+    message('The inverse has been calculated')
+    return(m)
+  }
+  else{
+    inverse = solve(x)
+    return(inverse)
+  }
 }
